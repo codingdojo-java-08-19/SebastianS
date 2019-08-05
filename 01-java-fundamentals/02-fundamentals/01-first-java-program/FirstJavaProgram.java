8bffad99027ead1a27ca31879698c4936846c091
ssaker18 <sebastian.c.saker.22@dartmouth.edu>
2019-08-05 16:09:01 -0500

    initial commit

---
+++ 01-java-fundamentals/02-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+        System.out.println("My name is Sebastian.");
+        System.out.println("I am 19 years old.");
+        System.out.println("I live in Chicago, IL.");
+    }
+}
