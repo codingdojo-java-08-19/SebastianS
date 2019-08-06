c55792540fa53d45ad92085f7f92210b7f434d95
ssaker18 <sebastian.c.saker.22@dartmouth.edu>
2019-08-06 15:55:30 -0500

    initial commit

---
+++ 01-java-fundamentals/02-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest{
+    public static void main(String[] args) {
+        FizzBuzz iD = new FizzBuzz();
+        String result = iD.fizzBuzz(7);
+        System.out.println(result);
+    }
+}
