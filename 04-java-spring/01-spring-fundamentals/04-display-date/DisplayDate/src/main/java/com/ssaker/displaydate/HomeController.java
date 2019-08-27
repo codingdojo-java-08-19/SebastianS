0f37ec18337066e97e9e87ef177be3cc837d251e
ssaker18 <sebastian.c.saker.22@dartmouth.edu>
2019-08-27 17:33:07 -0500

    initial commit

---
+++ 04-java-spring/01-spring-fundamentals/04-display-date/DisplayDate/src/main/java/com/ssaker/displaydate/HomeController.java
@@ -0,0 +1,42 @@
+package com.ssaker.displaydate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	@RequestMapping("/date")
+	public String date(Model model) {
+		Date date= new Date();
+		
+		SimpleDateFormat currDay= new SimpleDateFormat("EEEEE");
+		SimpleDateFormat currDate= new SimpleDateFormat("dd");
+		SimpleDateFormat currMonth= new SimpleDateFormat("MMMMM");
+		SimpleDateFormat currYear = new SimpleDateFormat("yyyy");
+		
+		String strDay= currDay.format(date);
+		String strDate = currDate.format(date);
+		String strMonth= currMonth.format(date);
+		String strYear= currYear.format(date);
+		
+		model.addAttribute("dateToView",strDay+ ", the "+strDate+" of "+strMonth+", "+strYear);
+		return "date.jsp";
+	}
+	@RequestMapping("/time")
+	public String time(Model model) {
+		Date time= new Date();
+		String timePattern="hh:mma";
+		SimpleDateFormat timeFormat= new SimpleDateFormat(timePattern);		String strTime = timeFormat.format(time);
+		model.addAttribute("timeToView",timeFormat.format(time));
+		
+		return "time.jsp";
+	}
+}
